3e743a3b10a306f96a26bd77494a35f984523d85
v1ku <vishnu.a.nair1995@gmail.com>
2023-05-02 02:25:26 +0530

    docker changes

--- stern/app/server.py
+++ stern/app/server.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+CORS(app)  # Allow any origin to access the API
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins='*')
 
